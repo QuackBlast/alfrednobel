d9a67bff0921af3151fef2263e4f1b7c96c836b1
acnenerd <145614880+acnenerd@users.noreply.github.com>
2024-03-23 13:21:11 +0100

    Update lol.c

--- lol.c
+++ lol.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main()
+return 0;
 {
 printf("Hello World! \n");
 printf("Hello World! \n");
 printf("Hello World! \n");
 printf("elllooo World! \n");
 printf("lololololol2");
+printf("rraweopjkrowaj");
 return 0;
 }
